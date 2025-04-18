ba6c3e0144ac85c2cde0cbaa111bca2ad3c77e05
Yash Parakh <153135288+YashParakh001@users.noreply.github.com>
2025-04-18 04:43:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,27 @@
 from flask import Flask, render_template, redirect, url_for
 import subprocess
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+   return render_template('index.html')
 
-@app.route('/track/<exercise>')
-def track_exercise(exercise):
-    # no more count logic!
-    return render_template(
-        'track.html',
-        exercise=exercise.capitalize()
-    )
 
 @app.route('/start/<exercise>')
 def start_exercise(exercise):
-    script_map = {
-        'plank': 'plank.py',
-        'pushup': 'pushup.py',
-        'situp': 'situps.py',
-        'squat': 'squat.py'
-    }
-    if exercise in script_map:
-        subprocess.Popen([
-            "/Users/Sid/.pyenv/versions/3.10.12/bin/python3.10",
-            script_map[exercise]
-        ])
-    return redirect(url_for('track_exercise', exercise=exercise))
+   script_map = {
+       'plank': 'plank.py',
+       'pushup': 'pushup.py',
+       'situp': 'situp.py',
+       'squat': 'squat.py'
+   }
+   if exercise in script_map:
+       subprocess.Popen(["python", script_map[exercise]])
+   return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
