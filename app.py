7a2697a069f93513e1820375501f3681b583e8cb
Siddharth-Aravind <sid.aravind23@gmail.com>
2025-04-18 03:41:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/track/<exercise>')
+def track_exercise(exercise):
+    # no more count logic!
+    return render_template(
+        'track.html',
+        exercise=exercise.capitalize()
+    )
+
 @app.route('/start/<exercise>')
 def start_exercise(exercise):
     script_map = {
         'plank': 'plank.py',
         'pushup': 'pushup.py',
         'situp': 'situps.py',
-        'squat': 'squats.py'
+        'squat': 'squat.py'
     }
     if exercise in script_map:
-        subprocess.Popen(["python", script_map[exercise]])
-    return redirect(url_for('index'))
+        subprocess.Popen([
+            "/Users/Sid/.pyenv/versions/3.10.12/bin/python3.10",
+            script_map[exercise]
+        ])
+    return redirect(url_for('track_exercise', exercise=exercise))
 
 if __name__ == '__main__':
     app.run(debug=True)
